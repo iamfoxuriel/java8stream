036cc53b067f847a855ad0278e73bd4937f39f1f
Jaden Zhuang <Jaden.Zhuang@ehealth-china.com>
2016-11-14 11:09:10 +0800

    add finder example

--- main/src/java/stream/Finder.java
+++ main/src/java/stream/Finder.java
@@ -3,8 +3,8 @@ package stream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Finder {
-    public static <E> List<E>  find(Iterable<E> collection, Matcher<E> m) {
+class Finder {
+    static <E> List<E>  find(Iterable<E> collection, Matcher<E> m) {
         List<E> result = new ArrayList<>();
         for (E item : collection) {
             if (m.matches(item)) {
